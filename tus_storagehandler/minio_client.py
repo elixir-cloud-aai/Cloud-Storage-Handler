6d9cb7937fdbd86f3c632b88b39f9bf3abbab2ec
Pratiksha Sankhe <sankhepratiksha3@gmail.com>
2024-08-27 23:48:25 +0530

    made small change
    Signed-off-by: Pratiksha Sankhe <sankhepratiksha3@gmail.com>

--- tus_storagehandler/minio_client.py
+++ tus_storagehandler/minio_client.py
@@ -1,7 +1,7 @@
-"""Minio Client Configuration Module.
+"""MinIO Client Configuration Module.
 
-This module initializes a Minio client using environment variables.
-It also provides a function to create a new Minio
+This module initializes a MinIO client using environment variables.
+It also provides a function to create a new MinIO
 bucket if it does not already exist.
 """
 
@@ -27,7 +27,7 @@ def create_bucket():
     """Creates a new bucket if it does not already exist.
 
     This function checks if the bucket specified by MINIO_BUCKET exists.
-    If it does not exist, it creates a new bucket using the Minio client instance.
+    If it does not exist, it creates a new bucket using the MinIO client instance.
     """
     if not minio_client.bucket_exists(MINIO_BUCKET):
         minio_client.make_bucket(MINIO_BUCKET)
