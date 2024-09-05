cbcf33021a3ca0c1a0d8abc69bfd3e635f6ac7e4
Pratiksha Sankhe <sankhepratiksha3@gmail.com>
2024-09-05 23:36:35 +0530

    fixed type issue
    Signed-off-by: Pratiksha Sankhe <sankhepratiksha3@gmail.com>

--- cloud_storage_handler/consts.py
+++ cloud_storage_handler/consts.py
@@ -1,27 +1,13 @@
-"""This module handles the loading of environment variables for MinIO configuration."""
-
-from typing import Dict
-
-from minio import Minio
+from typing import Any, Dict
 
 from cloud_storage_handler.minio_client import MinioClient
+from minio import Minio
 
-
-def load_minio_config() -> Dict[str, any]:
-    """Load MinIO configuration.
-
-    Returns:
-        Dict[str, any]: A dictionary containing MinIO configuration details.
-    """
-    minio_client = MinioClient()
-    return minio_client.get_minio_config()
-
-
-def initialize_minio_client(config: Dict[str, any]) -> Minio:
+def initialize_minio_client(config: Dict[str, Any]) -> Minio:
     """Initialize the MinIO client with the given configuration.
 
     Args:
-        config (Dict[str, any]): A dictionary containing configuration details.
+        config (Dict[str, Any]): A dictionary containing configuration details.
 
     Returns:
         Minio: A MinIO client instance.
@@ -34,13 +20,11 @@ def initialize_minio_client(config: Dict[str, any]) -> Minio:
         secure=config["is_secure"],
     )
 
-
 def main():
     """Main function to load configuration and initialize the MinIO client."""
     minio_client = MinioClient()
     minio_client.create_bucket(minio_client.bucket_name)
     print(minio_client.client)
     return minio_client.client
-
 
 minio_client = main()
