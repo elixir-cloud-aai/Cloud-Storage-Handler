58070c9ada309916979263f471bb3d541288f53f
Pratiksha Sankhe <sankhepratiksha3@gmail.com>
2024-09-05 23:40:18 +0530

    fixed lint issue
    Signed-off-by: Pratiksha Sankhe <sankhepratiksha3@gmail.com>

--- cloud_storage_handler/consts.py
+++ cloud_storage_handler/consts.py
@@ -1,7 +1,15 @@
+"""This module handles MinIO configuration and client initialization.
+
+It provides functions to load MinIO configuration, initialize the MinIO client
+with the configuration details, and perform operations such as bucket creation.
+"""
+
 from typing import Any, Dict
 
-from cloud_storage_handler.minio_client import MinioClient
 from minio import Minio
+
+from cloud_storage_handler.minio_client import MinioClient
+
 
 def initialize_minio_client(config: Dict[str, Any]) -> Minio:
     """Initialize the MinIO client with the given configuration.
@@ -20,11 +28,13 @@ def initialize_minio_client(config: Dict[str, Any]) -> Minio:
         secure=config["is_secure"],
     )
 
+
 def main():
     """Main function to load configuration and initialize the MinIO client."""
     minio_client = MinioClient()
     minio_client.create_bucket(minio_client.bucket_name)
     print(minio_client.client)
     return minio_client.client
+
 
 minio_client = main()
